8f7183ec90ac5fafdcc97436d73ff5a2d95b9403
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-09-28 17:29:57 +0900

    initial commit

---
+++ src/main/java/com/example/hhplus2week/Hhplus2weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhplus2week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhplus2weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hhplus2weekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hhplus2week/Hhplus2weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hhplus2week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hhplus2weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
